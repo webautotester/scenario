712c8f65b5e028c3cb4f734b02ebd4c8c77faa80
Xavier Blanc <xavier.blanc@labri.fr>
2017-08-30 15:17:20 +0200

    first version

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = {
-  entry: { 'src' : glob.sync('./src/*.js') },  
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'out')
-  }
+	entry: { 'src' : glob.sync('./src/*.js') },  
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'lib')
+	}
 };

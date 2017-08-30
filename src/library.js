9648534fcb60cdf929c2a5288d74132ac796feff
Xavier Blanc <xavier.blanc@labri.fr>
2017-08-30 15:59:59 +0200

    update npm

---
+++ src/library.js
@@ -0,0 +1,23 @@
+import * as ActionFactory from './ActionFactory';
+import GotoAction from './GotoAction';
+import ClickAction from './ClickAction';
+import CheckAction from './CheckAction';
+import MouseOverAction from './MouseOverAction';
+import TypeAction from './TypeAction';
+import ScrollToAction from './ScrollToAction';
+import WaitAction from './WaitAction';
+import BackAction from './BackAction';
+import Scenario from './Scenario';
+
+export {
+	GotoAction,
+	ClickAction,
+	CheckAction,
+	MouseOverAction,
+	TypeAction,
+	ScrollToAction,
+	WaitAction,
+	BackAction,
+	ActionFactory,
+	Scenario
+};

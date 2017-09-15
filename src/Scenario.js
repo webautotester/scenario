b720c247e46d42e1cc64d75586a5e01ebbe4fad4
Xavier Blanc <xavier.blanc@labri.fr>
2017-09-15 16:21:18 +0200

    test

--- src/Scenario.js
+++ src/Scenario.js
@@ -1,70 +1,70 @@
 import * as ActionFactory from './ActionFactory';
+import WaitAction from './WaitAction.js';
 
 export default class Scenario {
 
-    constructor(actionsJSON) {
-        this.actions = [];
-        this.index = undefined;
-        if (actionsJSON) {
-            actionsJSON.forEach(ac => {
-                this.addAction(ActionFactory.createAction(ac));
-        });
-        }
-    }
+	constructor(actionsJSON) {
+		this.actions = [];
+		if (actionsJSON) {
+			actionsJSON.forEach(ac => {
+				this.addAction(ActionFactory.createAction(ac));
+			});
+		}
+	}
 
-    toString() {
-        return `[${this.actions.join(', ')}]`;
-    }
+	toString() {
+		return `[${this.actions.join(', ')}]`;
+	}
 
-    toJSON() {
-        return JSON.stringify(this.actions);
-    }
+	toJSON() {
+		return JSON.stringify(this.actions);
+	}
 
-    addAction(action) {
-        this.actions.push(action);
-    }
+	addAction(action) {
+		this.actions.push(action);
+	}
 
-    addOrUpdateWait(waitTime) {
-        const newActions = [];
-        this.actions.filter((action) => action.type !== new WaitAction().type).forEach((action) => {
-            newActions.push(action);
-        newActions.push(new WaitAction(waitTime));
-    });
-        this.actions = newActions;
-    }
+	addOrUpdateWait(waitTime) {
+		const newActions = [];
+		this.actions.filter((action) => action.type !== new WaitAction().type).forEach((action) => {
+			newActions.push(action);
+			newActions.push(new WaitAction(waitTime));
+		});
+		this.actions = newActions;
+	}
 
-    get depth() {
-        return this.actions.length;
-    }
+	get depth() {
+		return this.actions.length;
+	}
 
-    duplicate() {
-        var dupication = new Scenario();
-        this.actions.forEach(ac => dupication.addAction(ac));
-        return dupication;
-    }
+	duplicate() {
+		var dupication = new Scenario();
+		this.actions.forEach(ac => dupication.addAction(ac));
+		return dupication;
+	}
 
-    equalsTo(scenario) {
-        if (this.actions.length === scenario.actions.length) {
-            for (var i = 0; i < this.actions.length; i++) {
-                if (! this.actions[i].equalsTo(scenario.actions[i])) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+	equalsTo(scenario) {
+		if (this.actions.length === scenario.actions.length) {
+			for (var i = 0; i < this.actions.length; i++) {
+				if (! this.actions[i].equalsTo(scenario.actions[i])) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    attachTo(browser) {
-        return attachTo(browser, this.actions);
-    }
+	attachTo(browser) {
+		return attachTo(browser, this.actions);
+	}
 }
 
 function attachTo(nightmare, actions) {
-    if (actions.length === 0) return nightmare;
-    else {
-        const currentAction = actions.shift();
-        return attachTo(currentAction.attachTo(nightmare) , actions);
-    }
+	if (actions.length === 0) return nightmare;
+	else {
+		const currentAction = actions.shift();
+		return attachTo(currentAction.attachTo(nightmare) , actions);
+	}
 }
---
+++ test/ScenarioTest.js
@@ -0,0 +1,75 @@
+
+const lib = require('../lib');
+const assert = require('assert');
+const Nightmare = require('nightmare');	
+
+const scenario = new lib.Scenario();
+const gotoAction = new lib.GotoAction('https://duckduckgo.com');
+const typeAction = new lib.TypeAction('#search_form_input_homepage','github nightmare');
+const clickAction = new lib.ClickAction('#search_button_homepage');
+const waitAction = new lib.WaitAction('#r1-0 a.result__a');
+scenario.addAction(gotoAction);
+scenario.addAction(typeAction);
+scenario.addAction(clickAction);
+scenario.addAction(waitAction);
+
+
+describe('Create Actions and Scenario', function () {
+	describe('new action', function() {
+		it ('should create new actions by using new()', function() {
+			var gta = new lib.GotoAction('https://duckduckgo.com');
+			assert(gta.url === 'https://duckduckgo.com');
+			assert(gta.type === 'GotoAction');
+
+			var ca = new lib.ClickAction('#search_button_homepage');
+			assert(ca.selector === '#search_button_homepage');
+			assert(ca.type === 'ClickAction');
+		});
+	});
+
+	describe('factory', function() {
+		it ('should create new actions by using the factory', function() {
+			var gta = lib.ActionFactory.createAction({type:'GotoAction', url:'https://duckduckgo.com'});
+			assert(gta.url === 'https://duckduckgo.com');
+			assert(gta.type === 'GotoAction');
+
+			var ca = lib.ActionFactory.createAction({type:'ClickAction',selector:'#search_button_homepage'});
+			assert(ca.selector === '#search_button_homepage');
+			assert(ca.type === 'ClickAction');
+		});
+	});
+});
+
+
+describe('Save and Load Scenario', function () {
+	it ('should save a scenario and load it', function() {
+		var json = scenario.toJSON();
+		var scenar = new lib.Scenario(JSON.parse(json));
+		assert(scenar.equalsTo(scenario));
+		
+
+	});
+});
+
+
+describe('Execute Actions', function () {
+	this.timeout(40000);
+	describe('Run Scenario', function() {
+		it('should run the scenario with chrome', function(done) {
+			var nightmare = new Nightmare({show:true});
+			scenario.attachTo(nightmare)
+				.evaluate( () => {
+
+				})
+				.end()
+				.then(() => {
+					assert(true);
+					done();
+				}) 
+				.catch ( (e) => {
+					assert(false);
+					done();
+				});
+		});
+	});
+});

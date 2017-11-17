91b75295880493c9f1ac22cb728acebdeced75cd
Xavier Blanc <xavier.blanc@labri.fr>
2017-11-17 15:29:26 +0100

    header

--- src/GotoAction.js
+++ src/GotoAction.js
@@ -1,13 +1,14 @@
 import Action from './Action';
 
 export default class GotoAction extends Action {
-	constructor(url) {
+	constructor(url, header) {
 		super();
 		this.url = url;
+		this.header = header || {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1'};
 	}
 
 	attachTo(browser) {
-		return browser.goto(this.url);
+		return browser.goto(this.url, this.header);
 	}
 
 	toString() {
